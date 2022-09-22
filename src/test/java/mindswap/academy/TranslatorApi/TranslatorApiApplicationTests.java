2544ec17e35079b147d77eb073d7f7e52ed900ec
HenriqueGuim <zpatins@gmail.com>
2022-09-22 16:19:23 +0100

    First Commit

---
+++ src/test/java/mindswap/academy/TranslatorApi/TranslatorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package mindswap.academy.TranslatorApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranslatorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
