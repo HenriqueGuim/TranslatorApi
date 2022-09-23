61602353644ae9a9d09be2da5ec602bc12ed882e
HenriqueGuim <zpatins@gmail.com>
2022-09-23 16:24:39 +0100

    removed 1 Enum page and added language Code

--- src/main/java/mindswap/academy/TranslatorApi/controller/TranslatorController.java
+++ src/main/java/mindswap/academy/TranslatorApi/controller/TranslatorController.java
@@ -2,9 +2,15 @@ package mindswap.academy.TranslatorApi.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import mindswap.academy.TranslatorApi.service.TranslatorService;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMapAdapter;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/translator")
@@ -16,7 +22,16 @@ public class TranslatorController {
     }
 
     @PostMapping()
-    public String getTranslator(@RequestParam(name = "text") String text, @RequestParam(name = "src_lang", required = false) String sourceLanguage, @RequestParam(name = "trg_lang") String languageToTranslate) throws JsonProcessingException, URISyntaxException {
-        return translatorService.getTranslator(sourceLanguage, languageToTranslate, text);
+    public ResponseEntity<?> getTranslator(@RequestParam(name = "text") String text, @RequestParam(name = "src_lang", required = false) String sourceLanguage, @RequestParam(name = "trg_lang") String languageToTranslate) throws JsonProcessingException, URISyntaxException {
+        String translation = translatorService.getTranslator(sourceLanguage, languageToTranslate, text);
+        if (translation == null){
+            String message = "Language not supported";
+            Map<String, String> response = new HashMap<>();
+            response.put("error", "Language not supported");
+
+            return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+        }
+
+        return ResponseEntity.ok(translation);
     }
 }
---
+++ src/main/java/mindswap/academy/TranslatorApi/utils/enums/Languages.java
@@ -0,0 +1,47 @@
+package mindswap.academy.TranslatorApi.utils.enums;
+
+public enum Languages {
+    BG( "Bulgarian", "BG"),
+    CS( "Czech", "CS"),
+    DA( "Danish", "DA"),
+    DE( "German", "DE"),
+    EL( "Greek", "EL"),
+    EN( "English", "EN"),
+    ES( "Spanish", "ES"),
+    ET( "Estonian", "ET"),
+    FI( "Finnish", "FI"),
+    FR( "French", "FR"),
+    HU( "Hungarian", "HU"),
+    ID( "Indonesian", "ID"),
+    IT( "Italian", "IT"),
+    JA( "Japanese", "JA"),
+    LT( "Lithuanian", "LT"),
+    LV( "Latvian", "LV"),
+    NL( "Dutch", "NL"),
+    PL( "Polish", "PL"),
+    PT( "Portuguese", "PT"),
+    RO( "Romanian", "RO"),
+    RU( "Russian", "RU"),
+    SK( "Slovak", "SK"),
+    SL( "Slovenian", "SL"),
+    SV( "Swedish", "SV"),
+    TR( "Turkish", "TR"),
+    UK( "Ukrainian", "UK"),
+    ZH( "Chinese", "ZH");
+    ;
+    private final String language;
+    private final String languageCode;
+
+    Languages(String language, String languageCode) {
+        this.language = language;
+        this.languageCode = languageCode;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public String getLanguageCode() {
+        return languageCode;
+    }
+}
